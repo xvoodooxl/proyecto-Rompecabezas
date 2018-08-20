2ce7f3b5bbb0b86212581d15620c0addf87f194b
Fermin Solis <xvoodooxl@gmail.com>
2018-08-20 14:28:10 -0300

    creada funcion para actualizar posicion vacia

--- js/juego.js
+++ js/juego.js
@@ -79,7 +79,8 @@ function intercambiarPosicionesGrilla(filaPos1, columnaPos1, filaPos2, columnaPo
 
 // Actualiza la posición de la pieza vacía
 function actualizarPosicionVacia(nuevaFila, nuevaColumna) {
-    //COMPLETAR
+  filaVacia = nuevaFila;
+  columnaVacia = nuevaColumna;
 }
 
 
